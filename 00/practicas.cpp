3c94d04c60a151f02a598a3b37d40dc5dcd90210
Jorge.Rodriguez-Moreno <Jorge.Rodriguez-Moreno@continental-corporation.com>
2015-06-15 14:21:52 -0500

    - Added glutKeyboardFunc() to close when ESC is pressed - Created macro to define window size

--- 00/practicas.cpp
+++ 00/practicas.cpp
@@ -1,12 +1,34 @@
 #include <GL/freeglut.h>
+#include <stdio.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_XPOS 300
+#define WINDOW_YPOS 300
+
+#define DEBUGMSG 1
+
+void preocessKey(unsigned char, int, int);
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitWindowPosition(300, 300);
-	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(WINDOW_XPOS, WINDOW_YPOS);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	glutCreateWindow("Mi primer ventana GLUT");
+
+	glutKeyboardFunc(preocessKey);
 	glutMainLoop();
 
 	return 0;
+}
+
+void preocessKey(unsigned char key, int x, int y)
+{
+#if DEBUGMSG
+	printf("Key press: %c\n", key);
+#endif
+	if( 27 == key){
+    	exit(0);
+	}
 }
