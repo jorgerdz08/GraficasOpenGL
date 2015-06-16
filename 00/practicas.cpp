0a86d457fbc96d2deceacb2de75e6fe87d68ca0f
Jorge.Rodriguez-Moreno <Jorge.Rodriguez-Moreno@continental-corporation.com>
2015-06-16 15:37:28 -0500

    - "Mi primera experiencia con shaders" - Reordered functions to compare versus Practica4.cpp from Moodle - Removed rectangle related functions

--- 00/practicas.cpp
+++ 00/practicas.cpp
@@ -1,8 +1,9 @@
+
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#include "utils/Utils.h"
 #include <stdio.h>
 #include <iostream>
+#include "utils/Utils.h"
 
 using namespace std;
 
@@ -11,47 +12,71 @@ using namespace std;
 #define WINDOW_XPOS 300
 #define WINDOW_YPOS 300
 
-#define DEBUGMSG 1
+#define DEBUGMSG 0
 
-void preocessKey(unsigned char, int, int);
-void processSpecialKey(int, int, int);
-
+void keyboardHandler(unsigned char, int, int);
 void display(void);
 void createTriangle(void);
-void createRectangle(void);
-void createRectangle_2Buffers(void);
+void initShaders(void);
 
 GLfloat trianglePos[] = { 0.0,  0.9,  1.0,
 						  -0.9, -0.9,  1.0,
 						   0.9, -0.9,  1.0
 };
 
-//								x,    y,   r,   g,   b
-GLfloat rectanglePosCol[] = { -0.7,  0.7, 0.9, 0.9, 0.0,
-							   -0.7, -0.7, 0.9, 0.0, 0.0,
-							    0.7,  0.7, 0.0, 0.0, 0.9,
-							    0.7, -0.7, 0.0, 0.9, 0.9
-};
+GLuint programId;
+GLuint vertexArrayId[1];
+GLint vertexPositionLoc;
 
-GLfloat rectanglePosCol_Blue[] = { -0.7,  0.7, 0.0, 0.0, 1.0,
-							   -0.7, -0.7, 0.0, 0.0, 1.0,
-							    0.7,  0.7, 0.0, 0.0, 1.0,
-							    0.7, -0.7, 0.0, 0.0, 1.0
-};
 
-GLfloat rectanglePos[] = {  -0.7,  0.7,
-							-0.7, -0.7,
-							 0.7,  0.7,
-							 0.7, -0.7
-};
+void keyboardHandler(unsigned char key, int x, int y){
+#if DEBUGMSG
+	printf("Key press: %c\n", key);
+#endif
+	if( 27 == key){
+    	exit(0);
+	}
+	else if( 'f' == key){
+		//glutEnterGameMode();	// Hangs when run on laptop HP ZBook
+		glutFullScreenToggle();
+	}
+}
 
-GLfloat rectangleCol[] = {  0.9, 0.9, 0,
-							0.9, 0,   0,
-							0, 	 0,   0.9,
-							0,   0.9, 0.9
-};
+void initShaders(){
+	GLuint vShaderId = Utils::compileShader("shaders/position.vsh", GL_VERTEX_SHADER);
+	if(!Utils::shaderCompiled(vShaderId)) return;
 
-GLuint vertexArrayId[2];
+	GLuint fShaderId= Utils::compileShader("shaders/orange.fsh", GL_FRAGMENT_SHADER);
+	if(!Utils::shaderCompiled(fShaderId)) return;
+
+	programId = glCreateProgram();
+	glAttachShader(programId, vShaderId);
+	glAttachShader(programId, fShaderId);
+	glLinkProgram(programId);
+
+	vertexPositionLoc= glGetAttribLocation(programId, "position");
+}
+
+void transferData(){
+	glGenVertexArrays(1, vertexArrayId);
+	glBindVertexArray(vertexArrayId[0]);
+	GLuint bufferId[1];
+	glGenBuffers(1, bufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferId[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(trianglePos), trianglePos, GL_STATIC_DRAW);
+	glVertexAttribPointer(vertexPositionLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vertexPositionLoc);
+}
+
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(programId);
+	glBindVertexArray(vertexArrayId[0]);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glutSwapBuffers();
+}
 
 int main(int argc, char** argv) {
 	int screenX, screenY;
@@ -66,109 +91,20 @@ int main(int argc, char** argv) {
 
 	glutInitWindowPosition(screenX, screenY);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutCreateWindow("Mi primera experiencia con shaders");
 
-	glutCreateWindow("Mi primer ventana GLUT");
-
-	glutKeyboardFunc(preocessKey);
-	glutSpecialFunc(processSpecialKey);
+	glutKeyboardFunc(keyboardHandler);
 	glutDisplayFunc(display);
 
 	glewInit();
 	if(GLEW_VERSION_4_0){
 		cout << "OpenGL 4.0 supported" << endl;
 	}
-	glClearColor(0.72, 0.28, 0.46, 1.0);
-	glGenVertexArrays(2, vertexArrayId);
-	createTriangle();
-	//createRectangle();
-	createRectangle_2Buffers();
+
+	initShaders();
+	transferData();
+	glClearColor(0.25, 0.80, 0.40, 1.0);
 
 	glutMainLoop();
-
 	return 0;
-}
-
-void preocessKey(unsigned char key, int x, int y){
-#if DEBUGMSG
-	printf("Key press: %c\n", key);
-#endif
-	if( 27 == key){
-    	exit(0);
-	}
-	else if( 'f' == key){
-		//glutEnterGameMode();	// Hangs when run on laptop HP ZBook
-		glutFullScreenToggle();
-	}
-}
-
-
-void processSpecialKey(int key, int x, int y) {
-	int keyModifier;
-
-#if DEBUGMSG
-	if(116 == key)
-		printf("Special key press: Alt + ");
-	else
-		printf("F%d\n", key);
-#endif
-
-	switch(key) {
-	case GLUT_KEY_F4 :
-		keyModifier = glutGetModifiers();
-		if (keyModifier == (GLUT_ACTIVE_ALT))
-			exit(0);
-		break;
-	}
-}
-
-void display(){
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBindVertexArray(vertexArrayId[0]);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glBindVertexArray(vertexArrayId[1]);
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-	glutSwapBuffers();
-}
-
-void createTriangle(){
-	glBindVertexArray(vertexArrayId[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, 1);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(trianglePos), trianglePos, GL_STATIC_DRAW);
-	glVertexPointer(3, GL_FLOAT, 0, 0);
-	glEnableClientState(GL_VERTEX_ARRAY);
-}
-
-void createRectangle(){
-#define TESTCOLORARRAY 0
-	GLuint bufferID[2];
-	glBindVertexArray(vertexArrayId[1]);
-	glGenBuffers(2, bufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, bufferID[0]);
-#if TESTCOLORARRAY
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectanglePosCol_Blue), rectanglePosCol_Blue, GL_STATIC_DRAW);
-#else
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectanglePosCol), rectanglePosCol, GL_STATIC_DRAW);
-#endif
-	glVertexPointer(3, GL_FLOAT, 5*sizeof(GLfloat), 0);
-	glEnableClientState(GL_VERTEX_ARRAY);
-
-	glColorPointer( 3, GL_FLOAT, 5*sizeof(GLfloat), (GLvoid*)(2*sizeof(GLfloat)) );
-	glEnableClientState(GL_COLOR_ARRAY);
-}
-
-void createRectangle_2Buffers(){
-
-	GLuint bufferID[2];
-	glBindVertexArray(vertexArrayId[1]);
-	glGenBuffers(2, bufferID);
-
-	glBindBuffer(GL_ARRAY_BUFFER, bufferID[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectanglePos), rectanglePos, GL_STATIC_DRAW);
-	glVertexPointer(2, GL_FLOAT, 0, 0);
-	glEnableClientState(GL_VERTEX_ARRAY);
-
-	glBindBuffer(GL_ARRAY_BUFFER, bufferID[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleCol), rectangleCol, GL_STATIC_DRAW);
-	glColorPointer(3, GL_FLOAT, 0, 0);
-	glEnableClientState(GL_COLOR_ARRAY);
 }
---
+++ 00/shaders/orange.fsh
@@ -0,0 +1,8 @@
+
+#version 330
+out vec4 pixelColor;
+
+void main() {
+	gl_FragColor = vec4(1.0, 0.7, 0.2, 1.0);
+	//pixelColor = vec4(1.0, 0.4, 0.2, 1.0);
+}
